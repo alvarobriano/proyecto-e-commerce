4a982e9fc728644b392f9c0c5e547b707e8a4bfd
sofiahubert <sofia.hubert.gonzalez@gmail.com>
2023-11-19 14:49:39 -0300

    Parte 2 y 3 de la entrega 8

--- Backend-Tarea8/app.js
+++ Backend-Tarea8/app.js
@@ -1,14 +1,26 @@
 const fs = require('fs');
 const path = require('path');
-
-const PORT = 3000;
-
 const cors = require('cors');
 const express = require('express');
+const jwt = require('jsonwebtoken');
+
 const app = express();
+const PORT = 3000;
+
+// Clave secreta para JWT - Debería estar en una variable de entorno
+const SECRET_KEY = 'tuClaveSecreta';
+
+// Usuarios de ejemplo (en una implementación real, deberías consultar una base de datos)
+const users = [
+    { username: 'user1', password: 'pass1' },
+    { username: 'user2', password: 'pass2' }
+];
 
 app.use(cors());
 app.options('*', cors());
+
+// Colocar express.json() aquí para analizar el cuerpo de las solicitudes POST
+app.use(express.json());
 
 // Manejador POST para 'cart'
 app.post('/cart', (req, res) => {
@@ -22,7 +34,7 @@ app.get('/cats', (req, res) => {
     servirArchivoJSON(rutaArchivo, res);
 });
 
-// Ruta para 'cats_product/:id'111111
+// Ruta para 'cats_products/:id'
 app.get('/cats_products/:id', (req, res) => {
     const id = req.params.id;
     const rutaArchivo = path.join(__dirname, 'emercado-api-main', 'cats_products', `${id}.json`);
@@ -49,13 +61,30 @@ app.post('/sell', (req, res) => {
     servirArchivoJSON(rutaArchivo, res);
 });
 
-// Ruta para 'users_cart/:id'
+// Ruta para 'user_cart/:id'
 app.get('/user_cart/:id', (req, res) => {
     const id = req.params.id;
     const rutaArchivo = path.join(__dirname, 'emercado-api-main', 'user_cart', `${id}.json`);
     servirArchivoJSON(rutaArchivo, res);
 });
 
+// Endpoint POST para '/login'
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // Verificar las credenciales del usuario
+    const user = users.find(u => u.username === username && u.password === password);
+    
+    if (!user) {
+        return res.status(401).send('Credenciales incorrectas');
+    }
+
+    // Generar el token JWT
+    const token = jwt.sign({ username: user.username }, SECRET_KEY, { expiresIn: '1h' });
+
+    // Enviar el token al cliente
+    res.json({ token });
+});
 
 // Función para servir archivos JSON
 function servirArchivoJSON(rutaArchivo, res) {
@@ -70,4 +99,28 @@ function servirArchivoJSON(rutaArchivo, res) {
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+//Esta es la parte 3 de la entrega 8 
+function verificarToken(req, res, next) {
+    // Obtener el token del encabezado de la solicitud
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(403).send('Se requiere un token para autenticación');
+    }
+
+    try {
+        // Verificar el token
+        const verificado = jwt.verify(token, SECRET_KEY);
+        // Añadir los datos del usuario decodificados a la solicitud
+        req.usuario = verificado;
+        next(); // Pasar al siguiente middleware o manejador de ruta
+    } catch (error) {
+        res.status(401).send('Token inválido o expirado');
+    }
+}
+// Aplicar el middleware verificarToken solo a esta ruta
+app.post('/cart', verificarToken, (req, res) => {
+    const rutaArchivo = path.join(__dirname, 'emercado-api-main', 'cart', 'buy.json');
+    servirArchivoJSON(rutaArchivo, res);
 });
